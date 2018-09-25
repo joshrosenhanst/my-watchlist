6d8652bcc30ff60bc6b331cc66d1fc695291edc3
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-09-25 14:13:28 -0400

    remove font from index.css since the bulma default looks better; remove index.css from index.js since its empty

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import MyWatchList from './MyWatchList';
 import registerServiceWorker from './registerServiceWorker';
 

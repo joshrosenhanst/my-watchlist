cb5859206b52c82485a00be1457cde6affa03377
Josh mac <joshrh88@gmail.com>
2018-10-07 14:22:20 -0400

    replace react-snap with react-snapshot, the former was having js issues

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import 'core-js/es6/map';
 import 'core-js/es6/set';
 
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import { render } from 'react-snapshot';
 import MyWatchList from './MyWatchList';
 import registerServiceWorker from './registerServiceWorker';
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-    hydrate(<MyWatchList />, rootElement);
-} else {
-    render(<MyWatchList />, rootElement);
-}
+render(
+    <MyWatchList />,
+    document.getElementById('root')
+);
 
 registerServiceWorker();

5a36ffb68f0413638a77898c3d64f230031b2d18
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-01-02 12:11:58 -0500

    replace react-snapshot references

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import 'core-js/es6/map';
 import 'core-js/es6/set';
 
 import React from 'react';
-import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom';
 import MyWatchList from './MyWatchList';
 import registerServiceWorker from './registerServiceWorker';
 
-render(
+ReactDOM.render(
     <MyWatchList />,
     document.getElementById('root')
 );
